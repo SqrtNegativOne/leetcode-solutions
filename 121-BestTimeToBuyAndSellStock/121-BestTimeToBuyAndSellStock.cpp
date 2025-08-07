ea6cd0da7ffdc00583b5de05e17d4606aef0a615
Sqrt-1 <121613377+SqrtNegativOne@users.noreply.github.com>
2025-08-07 12:51:07 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,13 @@
+// Last updated: 07/08/2025, 12:51:04
+class Solution {
+public:
+    int maxProfit(const vector<int>& prices) {
+        int min_cur_price = prices[0];
+        int max_profit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            max_profit = max(max_profit, prices[i]-min_cur_price);
+            min_cur_price = min(min_cur_price, prices[i]);
+        }
+        return max_profit;
+    }
+};
